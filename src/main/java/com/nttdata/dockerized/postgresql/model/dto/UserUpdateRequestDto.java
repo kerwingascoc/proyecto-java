f91c13f3f0925bdaba93ba649f2a143ce06f4063
Carlos D. Adrianzen Adanaque <carlos.adrianzen27972@gmail.com>
2025-09-04 13:26:30 -0500

    feat: add POM dependencies and User DTOs (UserSaveRequestDto, UserUpdateRequestDto)

--- src/main/java/com/nttdata/dockerized/postgresql/model/dto/UserUpdateRequestDto.java
+++ src/main/java/com/nttdata/dockerized/postgresql/model/dto/UserUpdateRequestDto.java
@@ -1,22 +1,17 @@
 package com.nttdata.dockerized.postgresql.model.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-
-
 @Getter
 @Setter
-public class UserSaveRequestDto {
+public class UserUpdateRequestDto {
 
-    @NotBlank(message = "El nombre no puede estar vacío")
     @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
     private String name;
 
-    @NotBlank(message = "El email no puede estar vacío")
     @Email(message = "El formato del email no es válido")
     private String email;
 }
