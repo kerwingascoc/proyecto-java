c6d9753762bc549ca5b58d3b3b32171497b2486d
Kerwin Gasco Castillo <kerwingascoc@gmail.com>
2025-09-09 04:17:19 -0500

    Revert "Update y delete"
    This reverts commit c8aa70a4566809b3b6b08bd8de152941af6b1e0c.

--- src/main/java/com/nttdata/dockerized/postgresql/service/UserService.java
+++ src/main/java/com/nttdata/dockerized/postgresql/service/UserService.java
@@ -11,8 +11,4 @@ public interface UserService {
     public User findById(Long id);
 
     public User save(User user);
-
-    public User update(User user);
-
-    public void delete(Long id);
- }
+}
